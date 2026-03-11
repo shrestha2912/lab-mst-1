de460bb83192b77c262f914b89e52a0e92c14403
shrestha2912 <shresthagupta612@gmail.com>
2026-03-11 11:18:56 +0530

    React notification system lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const addNotification = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+
+      <h1>Total Notifications: {count}</h1>
+
+      <button onClick={addNotification}>
+        Add Notification
+      </button>
+
     </div>
   );
 }
